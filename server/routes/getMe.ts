7b352f8515cbaeaf193ffe7dfac8d73824aeef25
kenjimorita <kenjimoritata@gmail.com>
2020-05-24 09:18:34 +0900

    fix error

--- server/routes/getMe.ts
+++ server/routes/getMe.ts
@@ -1,16 +1,16 @@
-import { ErrorHandler } from "../modules/error";
+// import { ErrorHandler } from "../modules/error";
 
 export const getMe = (req: any, res: any, next: any) => {
   try {
-    const { token } = req.body;
-    if (!token) {
-      throw new ErrorHandler("500", "トークンがありません");
-    }
+    // const { token } = req.body;
+    // if (!token) {
+    //   throw new ErrorHandler("500", "トークンがありません");
+    // }
     // db.user.fined
     res.send({
       status: 200,
       message: "ok",
-      user: { name: "kenji", email: "kenjimoritata@gmail.com", password: 1111 }
+      user: { name: "kenji", email: "kenjimoritata@gmail.com", password: 1111 },
     });
   } catch (error) {
     next(error);
